a0e8bb272eb355cf2c42c684e8f4a734b559b72b
rabeltester44 <55041225+rabeltester44@users.noreply.github.com>
2021-02-17 19:39:12 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 import os
 print("\n\033[32;1mUpdating Wordlist...\n\n\033[0m")
-os.system('mv wordlist.txt wordlist_lama.txt')
+os.system('mv wordlist.txt hasillistkcfinder.txt')
 os.system('wget https://raw.githubusercontent.com/rabeltester44/kcfinder/main/wordlist.txt --no-check-certificate')
 print("\033[32;1mUpdate Wordlist Success\n\033[0m")
